a963aeb59146e39ef49f77c5243c77b1efe0b479
Jaime Nunes Leal <jaimenunesleal@gmail.com>
2022-10-30 15:58:37 -0300

    feature: Operações de saque, deposito e consulta

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-let lista = [];
+const listaContas = [];
 const formCadastro = document.getElementById("form");
-const footer = document.getElementById('footer')
-
-
+const formOperacao = document.getElementById("formTransacao");
+const entrar = document.getElementById("entrar");
 
 function checkConta(password, passwordVerify) {
   return password === passwordVerify;
 }
 
 // Pega os valores dos campos de inputs a partir do evento passado como parametro
-// Coloca os valores em uma variavel no formato de objeto 
+// Coloca os valores em uma variavel no formato de objeto
 function capturarDados(event) {
   event.preventDefault();
 
@@ -32,86 +31,77 @@ function capturarDados(event) {
     conta: Math.floor(1000 + Math.random() * 9000),
     saldo: 0,
   };
-  limpaForm(formCadastro)
-  createNewform(conta) // passa a variavel como parametro para esta função
+  listaContas.push(conta);
+  alert(`Conta criada com sucesso, o número da sua conta é: ${conta.conta}`);
+  limpaForm(formCadastro);
+  getOperacao(conta); // passa a variavel como parametro para esta função
 }
 
 // Evento disparado ao clicar em enviar no formulário de cadastro
 formCadastro.addEventListener("submit", capturarDados);
+entrar.addEventListener("click", getOperacao);
 
-// Cria novos elementos e o adionam no DOM 
-function createNewform(conta){
-
-  const divider = document.createElement('hr')
-  const section = document.createElement('section')
-  const title = document.createElement('h3')
-  const form =  document.createElement('form')
-  section.classList.add('center')
-  form.id="formTransacaoformTransacao"
-  form.innerHTML = `
-  <h3>Acesse sua conta</h3>
-  <div>
-      <label for="">Operação *</label>
-      <select id="operacao">
-        <option value='1' disabled>Escolha uma operação</option>
-        <option value='2'>Saque</option>
-        <option value='3'>Depósito</option>
-        <option value='4'>Consulta saldo</option>
-      </select>
-  </div>
-  <div>
-      <label for="">Valor</label>
-      <input type="number" placeholder="a partir de R$ 10" min="10" id='saldo'>
-  </div>
-  <div>
-      <label for="">Conta *</label>
-      <input type="number" placeholder="99900">
-  </div>
-  <div>
-      <label for="">Senha</label>
-      <input type="password" placeholder="" minlength="8" maxlength="12" name="password">
-  </div>
-  <div class="btn-form">
-      <button type="reset" class="clean" type="button">LIMPAR</button>
-      <button type="submit">ENVIAR</button>
-  </div>
-  `
-  title.textContent = `Conta criada com sucesso, seu conta é ${conta.conta}`
-  section.append(title)
-  section.append(form)
-  footer.before(divider)
-  footer.before(section)
-
-  const getOption = document.getElementById('operacao')
-  const getSaldo= document.getElementById('saldo')
+function getOperacao() {
+  formOperacao.style.display = "block";
+  const getOption = document.getElementById("operacao");
+  const getSaldo = document.getElementById("saldo");
   // verifica se o option selecionado é a consulta e bloquei o campo de valor se for verdadeiro
-  getOption.addEventListener('change', ()=>{
-    if(getOption.value == 4){
-      getSaldo.disabled =true
-    }else{
-      getSaldo.disabled =false
+  getOption.addEventListener("change", () => {
+    if (getOption.value == 4) {
+      getSaldo.disabled = true;
+    } else {
+      getSaldo.disabled = false;
     }
-  })
+  });
+
+  const span = document.getElementsByClassName("close")[0];
+  span.onclick = function () {
+    formOperacao.style.display = "none";
+  };
   // dispara o evento no submit
-  form.addEventListener('submit', (event)=>{
-    event.preventDefault()
-    vericaConta(conta)
-    verificaOperacao()
-    chameOperacao()
-  })
+  formOperacao.addEventListener("submit", efetuarOperacao);
 }
 
-function vericaConta(conta){
-  console.log(conta)
-}
-function verificaOperacao(){
-  console.log('passou operacao')
-}
-function chameOperacao(){
-  console.log('chamou')
+function obterConta(conta) {
+  const contaCliente = listaContas.find((c) => conta === conta);
+  return contaCliente;
 }
 
-function limpaForm(form){
-  const elements = Array.from(form.elements)
-  elements.filter((items)=> items.tagName == "INPUT").map((input) => input.value = '')
+const validarConta = (conta, senha) => {
+  const contaCliente = obterConta(conta);
+  return contaCliente && contaCliente.senha === senha ? true : false;
+};
+
+const efetuarOperacao = (evento) => {
+  evento.preventDefault();
+  const conta = parseInt(evento.target.conta.value);
+  const senha = evento.target.senha.value;
+  const valor = parseInt(evento.target.valor.value);
+  const contaValida = validarConta(conta, senha);
+
+  if (contaValida) {
+    console.log(evento.target.operacao.value);
+    switch (evento.target.operacao.value) {
+      case "2":
+        sacar(conta, valor);
+        break;
+      case "3":
+        depositar(conta, valor);
+        break;
+      case "4":
+        consultarSaldo(conta);
+        break;
+      default:
+        alert("Operação inválida");
+    }
+  } else {
+    alert("Conta ou senha inválida");
+  }
+};
+
+function limpaForm(form) {
+  const elements = Array.from(form.elements);
+  elements
+    .filter((items) => items.tagName == "INPUT")
+    .map((input) => (input.value = ""));
 }
